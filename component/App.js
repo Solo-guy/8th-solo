868f93198b272b91f23474a7fd6037a0be88c22b
front end wizzard <101088369+Solo-guy@users.noreply.github.com>
2022-06-22 17:07:17 +0700

    bgan

---
+++ component/App.js
@@ -0,0 +1,16 @@
+import html from '../core.js'
+import Header from '../component/Header.js'
+import Footer from '../component/Footer.js'
+import TodoList from '../component/TodoList.js'
+
+function App() {
+    return html`
+    <section class="todoapp"> 
+        ${Header()}
+        ${TodoList()} 
+        ${Footer()}
+    </section>   
+    `
+}
+
+export default App 
